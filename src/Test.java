01364e1c4983c54daf5d1718a50e037c4e86c8a0
Sujata Mandekar <sujata.mandekar5@gmail.com>
2025-11-05 17:27:40 +0530

    pushed Test.java

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("push new data on git");
+		System.out.println("push new1 data on git");
+	}
+
+}
