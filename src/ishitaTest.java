7bb1adafc7095dd18ac7b06f15375898aaba8f90
Sujata Mandekar <sujata.mandekar5@gmail.com>
2025-11-06 16:36:17 +0530

    requested changes are done

--- src/ishitaTest.java
+++ src/ishitaTest.java
@@ -6,6 +6,10 @@ public class ishitaTest {
 		System.out.println("push new data on git");
 		System.out.println("push new1 data on git");
 		System.out.println("push new1 data on git- Ishita");
+		System.out.println("push new1 data on git");
+		System.out.println("push new1 data on git- Ishita");
+		System.out.println("push new1 data on git");
+		System.out.println("push new1 data on git- bjwbefwef");
 	}
 
 }
